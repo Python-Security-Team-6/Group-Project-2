922ad70586a7c2877946c51f2355d9ca77efdb95
Soroush Keyana <soroush.keyana@gmail.com>
2023-11-22 13:03:43 +0100

    created feed page

---
+++ venv/socialmedia/feed/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+def feed(request):
+    posts = Post.objects.all()
+    return render(request, 'feed.html', {'posts': posts})
+
