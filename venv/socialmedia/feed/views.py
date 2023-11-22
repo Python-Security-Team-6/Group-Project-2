6abee01f346f68b91b48a4133b5edb0d4c145c5a
Soroush Keyana <soroush.keyana@gmail.com>
2023-11-22 14:54:36 +0100

    new features added

--- venv/socialmedia/feed/views.py
+++ venv/socialmedia/feed/views.py
@@ -3,6 +3,6 @@ from .models import Post
 
 # Create your views here.
 def feed(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-created_at')
     return render(request, 'feed.html', {'posts': posts})
 
