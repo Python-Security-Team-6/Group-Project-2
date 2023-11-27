a854d6ae964566e62137a1a7a39b6ef58110b96b
Isak Sahlstrand <42635890+Isse2111@users.noreply.github.com>
2023-11-27 14:20:34 +0100

    Revert "Completed the sprint1"

--- venv/socialmedia/chat/apps.py
+++ venv/socialmedia/chat/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RegisterLoginLogoutConfig(AppConfig):
+class ChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'register_login_logout'
+    name = 'chat'
